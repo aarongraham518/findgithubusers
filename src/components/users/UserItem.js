fc3a6eebbaabaeea22869a7c395c4e37a012843e
Aaron Graham <aarongraham518@gmail.com>
2020-08-26 13:59:26 -0400

    converted class components to function components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-    render() {
-
-        const {login, avatar_url, html_url} = this.props.user;
+//Destructoring values from user object in User component
+const UserItem = ({user: {login, avatar_url, html_url} }) => {        
         return (
             <div className='card text-center'>
                 <img 
@@ -22,8 +21,10 @@ class UserItem extends Component {
                     </a>
                 </div>
             </div>
-        )
-    }
-}
+        );
+};
 
-export default UserItem
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
+}
+export default UserItem;
