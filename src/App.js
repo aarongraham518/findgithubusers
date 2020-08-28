b9c36904fa0264c9414ead180bd890e9ce3fa0bd
Aaron Graham <aarongraham518@gmail.com>
2020-08-28 00:32:24 -0400

    Added Repos and RepoItem Components and added Data from api

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ class App extends Component{
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
-    alert: null
+    alert: null    
   }
   
   //Data received from Search.js 
@@ -43,6 +44,18 @@ class App extends Component{
     this.setState({user: res.data, loading: false});
   }
 
+  //Get users repos
+  getUserRepos = async username => {
+    this.setState({loading: true});
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({repos: res.data, loading: false});
+  }
+
   /*Method for this.clearUsers, sent up from Search.js Component*/
   //clear users from state
   clearUsers = () => this.setState({users: [], loading: false});
@@ -58,7 +71,7 @@ class App extends Component{
 
   render(){
     //destructoring values from this.state to avoid coding "this.state.users"
-    const {users, user, loading} = this.state;
+    const {users, user, repos, loading} = this.state;
     return (
       <Router>
       <div className="App">
@@ -80,7 +93,14 @@ class App extends Component{
           />
           <Route exact path='/about' component={About}/>
           <Route exact path='/user/:login' render={props =>(
-            <User {...props} getUser={this.getUser} user={user} loading={loading}/>
+            <User 
+              {...props}
+              getUser={this.getUser}
+              getUserRepos={this.getUserRepos}
+              user={user}
+              repos={repos}
+              loading={loading}
+            />
           )} />
           </Switch>
           
